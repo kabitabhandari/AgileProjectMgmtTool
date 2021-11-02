9ca1cb13f561ab0a1295bd605db1853ce66f36ff
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2021-11-02 18:32:54 -0500

    chore: MYSQL connection wired up

--- PPMToolFullStack/src/main/java/com/advance/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/advance/ppmtool/services/ProjectService.java
@@ -6,8 +6,6 @@ import com.advance.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Locale;
-
 @Service
 public class ProjectService {
     @Autowired
