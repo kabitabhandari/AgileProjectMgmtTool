d6e2f8ed6545e97410026af394c894782bf45bbf
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2021-11-02 19:27:42 -0500

    added two routes with keyword exact

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -4,17 +4,20 @@ import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
+import { Component } from "react";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Dashboard />
-        <Route path="/addProject" component={AddProject} />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
