413d41ce198eb646dd07e366ba21c4f448f6c482
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2021-11-05 12:15:04 -0500

    chore: hooked create a project in ui with backend

---
+++ ppmtool-react-client/src/actions/types.js
@@ -0,0 +1,2 @@
+//Define the type of action we are going to perform
+export const GET_ERRORS = "GET_ERRORS";
--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -1,4 +1,7 @@
 import { combineReducers } from "redux";
+import errorReducer from "./errorReducer";
 
 // we are exporting combineReducers, which is going to take parameter as an object that contain all reducers that we are going to create moving forward.
-export default combineReducers({});
+export default combineReducers({
+  errors: errorReducer,
+});
